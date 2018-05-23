0b4e618cbccd63c315cc03ab915ec548c9a07c8d
solomko2 <solomko2006@gmail.com>
2018-05-23 19:52:35 +0300

    added flow js to project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -12,17 +14,21 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+const root = document.getElementById('root');
 
+if(!(root instanceof Element)) {
+    throw 'Invalif root'
+}
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+    link: httpLink,
+    cache: new InMemoryCache()
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
-  , document.getElementById('root')
+  , root
 );
 registerServiceWorker();
